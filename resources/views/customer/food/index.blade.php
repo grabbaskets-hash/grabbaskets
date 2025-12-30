5d6974f9ed395d8b01f991e5843185965b2b9e22
GrabBasket Developer <developer@grabbasket.com>
2025-12-30 21:16:10 +0530

    update the customers index

--- resources/views/customer/food/index.blade.php
+++ resources/views/customer/food/index.blade.php
@@ -597,11 +597,7 @@
             @forelse($foods as $food)
                 <a href="{{ route('customer.food.details', $food->id) }}" class="premium-food-card">
                     <div class="card-img-container">
-                        @if(!empty($food->images) && is_array($food->images))
-                            <img src="{{ $food->images[0] }}" onerror="this.src='https://via.placeholder.com/480x300?text=Delicious+Food'">
-                        @else
-                            <img src="https://via.placeholder.com/480x300?text=Delicious+Food" />
-                        @endif
+                        <img src="{{ $food->first_image_url }}" onerror="this.src='https://via.placeholder.com/480x300?text=Delicious+Food'" alt="{{ $food->name }}">
                         
                         <div class="img-overlay-gradient"></div>
                         
